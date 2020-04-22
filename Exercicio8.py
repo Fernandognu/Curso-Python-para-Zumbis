30f5b6f53b00cc5c41e4f358180c4a27870c26f5
Fernando Garcia <fernandogdac@gmail.com>
2020-04-22 18:41:04 -0300

    Exercicio 8
     Faça agora o contrário, de Fahrenheit para Celsius.

---
+++ Exercicio8.py
@@ -0,0 +1,6 @@
+## Faça agora o contrário, de Fahrenheit para Celsius. ##
+
+fahrenheit = float(input('Temperatura em Fahrenheit: '))
+celsius = (fahrenheit - 32) * 5 / 9
+
+print (f'{fahrenheit:.1f}° Fahrenheit é igual a {celsius:.1f}° Celsius.')
