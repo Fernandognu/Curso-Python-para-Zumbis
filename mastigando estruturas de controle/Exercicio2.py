7b5d707e27ded38099bd2649884598c8a71cb6cb
Fernando Garcia <fernandogdac@gmail.com>
2020-04-23 13:46:35 -0300

    exercicio2
    Codigo otimizado

--- mastigando estruturas de controle/Exercicio2.py
+++ mastigando estruturas de controle/Exercicio2.py
@@ -2,10 +2,20 @@ minutos = int(input("Quantos minutos: "))
 
 if minutos < 200:
     valor = minutos * 0.20
-    print (f"Valor da conta R$ {valor}")
+    print (f"Valor da conta R$ {valor:.2f}")
 if 200 <= minutos < 400:
     valor = minutos * 0.18
-    print (f"Valor da conta R$ {valor}")
+    print (f"Valor da conta R$ {valor:.2f}")
 if minutos >= 400:
     valor = minutos * 0.15
-    print (f"Valor da conta R$ {valor}")
+    print (f"Valor da conta R$ {valor:.2f}")
+
+
+if minutos < 200:
+    preço = 0.20
+else:
+    if minutos <= 400:
+        preço = 0.18
+    else:
+        preço = 0.15
+print (f"Valor da conta R$ {minutos * preço:.2f}")
