dbca33bcb1a7b746b15bc487e6a22d75945f83f3
Fernando Garcia <fernandogdac@gmail.com>
2020-04-23 17:53:27 -0300

    Create Exercicio2.py
    Determine se um ano é bissexto. Verifique no Google como fazer isso...

---
+++ mastigando estruturas de controle/Exercicios/Exercicio2.py
@@ -0,0 +1,7 @@
+'''Determine se um ano é bissexto. Verifique no Google como fazer isso...'''
+
+ano = int(input("Digite um Ano: "))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print("Ano Bissexto!")
+else:
+    print("Não é um ano Bissexto!")
