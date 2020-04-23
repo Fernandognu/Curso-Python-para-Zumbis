5f41e10634b4079836591ef99499ef1e7867c811
Fernando Garcia <fernandogdac@gmail.com>
2020-04-23 15:24:13 -0300

    Update Exercicio10.py

--- mastigando estruturas de controle/Exercicio10.py
+++ mastigando estruturas de controle/Exercicio10.py
@@ -14,7 +14,7 @@ print ("Fatorial de 10: %d" %produto)
 
 n = 1
 produto = 1
-while n <= 9:
-    n = n + 1
+while n <= 10:
     produto = produto * n
+    n = n + 1
 print ("Fatorial de 10: %d" %produto)
